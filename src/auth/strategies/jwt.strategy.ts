cd1bfceb6c5cd82423205130d2c8907038ec5f2d
Abdulsamad Gbadebo <gbadebosamad0@gmail.com>
2025-08-22 01:49:00 +0100

    refactor JwtStrategy to include seller profile retrieval and improve user validation logic

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,12 +4,19 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from 'src/user/user.service';
+import { SellerService } from 'src/seller/seller.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Seller } from 'src/seller/entities/seller.entity';
+import { Repository } from 'typeorm';
+import { Role } from '../roles/roles.enum';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     config: ConfigService, 
-    private userService: UserService
+    private userService: UserService,
+    private readonly sellerService: SellerService,
+    @InjectRepository(Seller) private readonly sellerRepository: Repository<Seller>
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -22,9 +29,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   // fetch other user properties after validating the JWT
   const user =  await this.userService.findOne(payload.sub);
 
-  // return the full user object without paassword and tim
-  const { createdAt, updatedAt, ...rest} = user;
-  return rest;
+  // get seller id
+  if (user.roles.includes(Role.SELLER)) {
+    const sellerProfile = await this.sellerRepository.findOne({ where: { user: { id: user.id } }, relations: ['user', 'addresses'] });
+  return sellerProfile
+  }
+  
+
+  // return the full user object without paassword and timestamps
+  // const { createdAt, updatedAt, ...rest} = user;
+  return 'good boy';
   
   }
 }
