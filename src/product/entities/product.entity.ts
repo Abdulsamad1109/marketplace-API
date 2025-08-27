6756e5ee02344a30e274c50e66d1ef9f9251223e
Abdulsamad Gbadebo <gbadebosamad0@gmail.com>
2025-08-27 23:58:09 +0100

    add product entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  // Store image as a URL or file path
+  @Column({ nullable: true })
+  image: string;
+}
