7b79e8b2290543f06a3aad5c070b4522d73e623b
Abdulsamad Gbadebo <gbadebosamad0@gmail.com>
2025-08-27 23:57:19 +0100

    add product dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,3 +1,18 @@
+import { IsString, IsNumber, IsOptional, IsPositive, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
-    
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  // image URL or file path
+  @IsOptional()
+  @IsString()
+  image?: string;
 }
