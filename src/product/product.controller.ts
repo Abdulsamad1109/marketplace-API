3b54c925c240cb35ae43e0ee3da71471f0394c5c
abdulsamad <gbadebosamad0@gmail.com>
2025-09-18 14:05:46 -1100

    feat: enhance product creation to support multiple images and validate category/seller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiBearerAuth } from '@nestjs/swagger';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @ApiBearerAuth('access-token')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @UseInterceptors(FileInterceptor('image')) // expecting `image` field in form-data
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  @UseInterceptors(FilesInterceptor('images'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Create product with images',
+    type: CreateProductDto,
+  })
+  async create(
+    @Body() createProductDto: CreateProductDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    return this.productService.create(createProductDto, files);
   }
 
   @Get()
@@ -21,18 +29,18 @@ export class ProductController {
     return this.productService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.productService.findOne(id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(id, updateProductDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  //   return this.productService.update(id, updateProductDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.productService.remove(id);
+  // }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,42 +1,169 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { Category } from 'src/category/entities/category.entity';
+import { Seller } from 'src/seller/entities/seller.entity';
+import { Image } from 'src/image/entities/image.entity';
 import { CreateProductDto } from './dto/create-product.dto';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { UpdateProductDto } from './dto/update-product.dto';
 
-@Injectable()
+@Injectable ()
 export class ProductService {
-  constructor(
-    @InjectRepository(Product)
-    private readonly productRepository: Repository<Product>,
-  ) {}
+constructor(
+  @InjectRepository(Product) private readonly productRepository: Repository<Product>,
+  @InjectRepository(Category) private readonly categoryRepository: Repository<Category>,
+  @InjectRepository(Seller) private readonly sellerRepository: Repository<Seller>,
+  @InjectRepository(Image) private readonly imageRepository: Repository<Image>, 
+  private readonly cloudinaryService: CloudinaryService,
+) {}
 
-  async create(createProductDto: CreateProductDto): Promise<Product> {
-    const product = this.productRepository.create(createProductDto);
-    return await this.productRepository.save(product);
+async create(
+  createProductDto: CreateProductDto,
+  files: Express.Multer.File[],
+): Promise<Product> {
+  const { name, description, price, stock, categoryId, sellerId } =
+    createProductDto;
+
+  // 1. Find related category
+  const category = await this.categoryRepository.findOneBy({ id: categoryId });
+  if (!category) throw new BadRequestException('Invalid category');
+
+  // 2. Find related seller
+  const seller = await this.sellerRepository.findOneBy({ id: sellerId });
+  if (!seller) throw new BadRequestException('Invalid seller');
+
+  // 3. Upload images to Cloudinary
+  const imageEntities: Image[] = [];
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const uploadResult = await this.cloudinaryService.uploadFile(file);
+
+    const image = this.imageRepository.create({
+      url: uploadResult.secure_url,
+      publicId: uploadResult.public_id,
+      isMain: i === 0, // first image is main by default
+    });
+
+    imageEntities.push(image);
   }
+
+  // 4. Create product with images
+  const product = this.productRepository.create({
+    name,
+    description,
+    price,
+    stock,
+    category,
+    seller,
+    images: imageEntities,
+  });
+
+  return this.productRepository.save(product);
+}
+
 
   async findAll(): Promise<Product[]> {
-    return await this.productRepository.find();
-  }
-
-  async findOne(id: string): Promise<Product> {
-    const product = await this.productRepository.findOne({ where: { id } });
-    if (!product) {
-      throw new NotFoundException(`Product not found`);
+      return await this.productRepository.find();
     }
-    return product;
-  }
- 
-  async update(id: string, updateProductDto: UpdateProductDto): Promise<Product> {
-    const product = await this.findOne(id);
-    Object.assign(product, updateProductDto);
-    return await this.productRepository.save(product);
-  }
 
-  async remove(id: string): Promise<void> {
-    const product = await this.findOne(id);
-    await this.productRepository.remove(product);
-  }
+    // async findOne(id: number): Promise<Product> {
+    //   const product = await this.productRepository.findOne({ where: { id } });
+    //   if (!product) {
+    //     throw new NotFoundException(`Product not found`);
+    //   }
+    //   return product;
+    // }
+  
+    // async update(id: number, updateProductDto: UpdateProductDto): Promise<Product> {
+    //   const product = await this.findOne(id);
+    //   Object.assign(product, updateProductDto);
+    //   return await this.productRepository.save(product);
+    // }
+
+    // async remove(id: number): Promise<void> {
+    //   const product = await this.findOne(id);
+    //   await this.productRepository.remove(product);
+    // }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Injectable, NotFoundException } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { Repository } from 'typeorm';
+// import { Product } from './entities/product.entity';
+// import { CreateProductDto } from './dto/create-product.dto';
+// import { UpdateProductDto } from './dto/update-product.dto';
+
+// @Injectable()
+// export class ProductService {
+//   constructor(
+//     @InjectRepository(Product)
+//     private readonly productRepository: Repository<Product>,
+//   ) {}
+
+//   async create(createProductDto: CreateProductDto): Promise<Product> {
+//     const product = this.productRepository.create(createProductDto);
+//     return await this.productRepository.save(product);
+//   }
+
+//   async findAll(): Promise<Product[]> {
+//     return await this.productRepository.find();
+//   }
+
+//   async findOne(id: number): Promise<Product> {
+//     const product = await this.productRepository.findOne({ where: { id } });
+//     if (!product) {
+//       throw new NotFoundException(`Product not found`);
+//     }
+//     return product;
+//   }
+ 
+//   async update(id: number, updateProductDto: UpdateProductDto): Promise<Product> {
+//     const product = await this.findOne(id);
+//     Object.assign(product, updateProductDto);
+//     return await this.productRepository.save(product);
+//   }
+
+//   async remove(id: number): Promise<void> {
+//     const product = await this.findOne(id);
+//     await this.productRepository.remove(product);
+//   }
+// }
