40781bb8a0a460e48636476b326a0f80ad911d5e
abdulsamad <gbadebosamad0@gmail.com>
2025-10-15 17:35:05 -1100

    feat: add TypeOrmModule imports and exports for CartItem, Cart, and Product entities

--- src/cart-item/cart-item.module.ts
+++ src/cart-item/cart-item.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CartItemService } from './cart-item.service';
 import { CartItemController } from './cart-item.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartItem } from './entities/cart-item.entity';
+import { Cart } from 'src/cart/entities/cart.entity';
+import { Product } from 'src/product/entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CartItem, Cart, Product])],
   controllers: [CartItemController],
   providers: [CartItemService],
+  exports: [CartItemService, TypeOrmModule],
 })
 export class CartItemModule {}
