1a650a703fd9ec308b8c44ecdeb6c0073131fd9c
abdulsamad <gbadebosamad0@gmail.com>
2025-11-17 19:40:14 -1100

    feat: remove UpdatePaymentDto and add PaymentResponseDto for payment response handling

---
+++ src/payment/dto/payment-response.dto.ts
@@ -0,0 +1,9 @@
+export class PaymentResponseDto {
+  success: boolean;
+  message: string;
+  data?: {
+    authorization_url?: string;
+    access_code?: string;
+    reference: string;
+  };
+}
--- src/payment/dto/update-payment.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePaymentDto } from './initialize-payment.dto';
-
-export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
