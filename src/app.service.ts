44239fe3ab13948b1a2f6a278d197636ec2f2eec
abdulsamad <gbadebosamad0@gmail.com>
2025-11-28 13:50:48 -1100

    modify the app service file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello Y'all";
+    return "Hello, you're using the Marketplace API!";
   }
 }
